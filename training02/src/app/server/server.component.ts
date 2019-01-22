f5f517d10ea4195c606646b970231aca972d566c
Adam Hegedus <Adam_Hegedus@epam.com>
2019-01-22 16:15:03 +0100

    Change server status to randomly assign online/offline

--- training02/src/app/server/server.component.ts
+++ training02/src/app/server/server.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
     serverId = 10;
-    serverStatus = 'offline';
+    serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
 }
