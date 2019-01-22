0686749093f719a1627986754a3887f090d181b3
Adam Hegedus <Adam_Hegedus@epam.com>
2019-01-22 16:18:42 +0100

    Add ngStyle to change background of server statuses

--- training02/src/app/server/server.component.ts
+++ training02/src/app/server/server.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 10;
     serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
