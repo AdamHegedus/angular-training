a6cadd56053fd91723912a42e3c8abecfa9e966d
Adam Hegedus <Adam_Hegedus@epam.com>
2019-01-31 13:56:55 +0100

    Add recipe list structure

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Array<Recipe> = [
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+    )
+  ];
 
   constructor() { }
 
