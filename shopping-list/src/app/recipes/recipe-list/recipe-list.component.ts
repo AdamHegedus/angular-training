2de8c0608ee7557d27863818f436c527482577ea
Adam Hegedus <Adam_Hegedus@epam.com>
2019-01-31 14:21:28 +0100

    Add dynamic content to recipe list

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'Test Recipe',
       'Test Description',
       'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+    ),
+    new Recipe(
+      'New Recipe',
+      'New Description',
+      'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg'
     )
   ];
 
