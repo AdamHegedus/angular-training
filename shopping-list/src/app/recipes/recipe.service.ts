84ecda3ce2730954f76679a3ece5e31e0513404d
Adam Hegedus <Adam_Hegedus@epam.com>
2019-02-20 11:20:13 +0100

    Fix ingredients block in the recipe detail page

--- shopping-list/src/app/recipes/recipe.service.ts
+++ shopping-list/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -7,12 +8,22 @@ export class RecipeService {
         new Recipe(
             'Test Recipe',
             'Test Description',
-            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
         ),
         new Recipe(
             'New Recipe',
             'New Description',
-            'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg'
+            'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Salad', 1),
+                new Ingredient('Meat', 2),
+                new Ingredient('French Fries', 20)
+            ]
         )
     ];
 
